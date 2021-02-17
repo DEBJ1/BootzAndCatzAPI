27cb9cd783cb019de9348d158a6143191fcda0a8
annajoyw <arealjoyw@gmail.com>
2021-02-16 21:14:50 -0500

    pet class

--- BootzAndCatz.Data/Pet.cs
+++ BootzAndCatz.Data/Pet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace BootzAndCatz.Data
 {
     public class Pet
     {
+        [Key]
+        public Guid PetId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Breed { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string AboutMe { get; set; }
+        //FK to adoption agency ID
     }
 }
