1942b6fbbf5f05fa211e5e9b79033600367a38c5
annajoyw <arealjoyw@gmail.com>
2021-02-18 19:14:53 -0500

    added cat POCO

--- BootzAndCatz.Data/Pet.cs
+++ BootzAndCatz.Data/Pet.cs
@@ -7,16 +7,21 @@ using System.Threading.Tasks;
 
 namespace BootzAndCatz.Data
 {
+    //base class
     public class Pet
     {
         [Key]
         public Guid PetId { get; set; }
+
         [Required]
         public string Name { get; set; }
+
         [Required]
         public string Breed { get; set; }
+
         [Required]
         public int Age { get; set; }
+
         [Required]
         public string AboutMe { get; set; }
         //FK to adoption agency ID
