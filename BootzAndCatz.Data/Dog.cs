eb0992c37f3e2d3378a122670e8b10a1d7bea026
DEBJ1 <14balmerd@gmail.com>
2021-02-18 19:29:56 -0500

    Dog Data

--- BootzAndCatz.Data/Dog.cs
+++ BootzAndCatz.Data/Dog.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BootzAndCatz.Data
 {
-    public class Dog
+    public class Dog : Pet
 
     {
+        [Key]
+        public int DogId { get; set; }
+
+        [Required]
+        public bool IsChipped { get; set; }
+
+        [Required]
+        public string EnergyLevel { get; set; }
+
+        [Required]
+        public string Size { get; set; }
     }
 }
+
